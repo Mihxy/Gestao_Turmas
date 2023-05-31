5f0697ff0ff859a52e0a15dfa0e8a2d940c44aa7
Daniel <Daniel@DESKTOP-14875C6>
2023-05-31 11:15:53 +0100

    Adicionar arquivos de projeto.

---
+++ Gestao_Turmas/C_Alunos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestao_Turmas
+{
+    public class C_Alunos
+    {
+        public string? Nome { get; set; }
+        public DateTime DtNascimento { get; set; }
+        public string? Genero { get; set; }
+        public string? Email { get; set; }
+        public string? Contacto { get; set; }
+        public int NumAluno { get; set; }
+    }
+}
